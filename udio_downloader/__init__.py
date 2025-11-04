14ca2dc57b8d038f9cf479f83429cb6ef4e794e0
Mitchell Local <idiovoidi@gmail.com>
2025-11-05 01:28:33 +1100

    Remove old Python implementation, keeping Chrome extension only

--- udio_downloader/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Udio Downloader - A utility to download all songs and folders from Udio platform.
-"""
-
-__version__ = "1.0.0"
-__author__ = "Udio Downloader Team"
--- udio_downloader/models/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Data models for the Udio Downloader.
-"""
-
-from .song_info import SongInfo
-from .folder_node import FolderNode
-from .download_config import DownloadConfig
-
-__all__ = ['SongInfo', 'FolderNode', 'DownloadConfig']
